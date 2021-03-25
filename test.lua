b2bc306da362609adc01c5c3da201ce2c158d09f
Paragon <96paragon@gmail.com>
2021-03-25 09:55:58 +0100

    Practicing

--- test.lua
+++ test.lua
@@ -1,1 +1,63 @@
-print("hello world")
+
+--print("hello world")
+
+speedy = 'Paragon'
+numero = 10
+number = 20
+boolean = true
+
+--[[print(speedy .. ' has ' .. numero .. ' Apples. ')
+print(numero - number)
+print(boolean)
+]]
+
+--[[if boolean then
+print("True")
+else
+print("False")
+end
+
+if numero == 10 then
+print("10 Is Correct")
+end
+
+if number > 10 then
+print("Number is greater than 10")
+end
+]]
+
+--[[function test()
+print("Running Function")
+end
+
+test()
+
+function test2(x)
+print ("Test2: " .. x)
+end
+
+test2(numero)
+]]
+
+-- Apples, Grapes, Chex Mix, Ice Cream
+
+--[[array = {}
+
+table.insert(array, "Apples")
+print(array[1])
+table.insert(array, "Grapes")
+print(array[2])
+
+table.remove(array, 2)
+print(array[1])
+]]
+
+for i = 1,20 do
+  print(i)
+end
+
+loop = 0
+while loop <= 10 do
+  print("Loop: " .. loop)
+  loop = loop + 1
+end
